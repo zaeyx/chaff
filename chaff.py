9fb84e3c1ecf162a027eb71bfdfc44f5ace27b85
zaeyx <Accipitercaelum@gmail.com>
2013-05-08 13:14:17 -0600

    Proof of Concept Complete
    This proof of concept demonstrates the idea behind chaff.
    
    Password policy doesn't always stop users from picking horrible passwords.
    
    Chaff cloaks the password hash of the user among tons of other hashes.  All hashes work to authenticate the user, but all the hashes other than the one containing the users chosen password are randomly generated to be computationally unfeasible to crack.
    
    In this way, all the hashes working means there is no indication of which one is the user's chosen password.  The user's chosen password is the only one possible to crack.
    
    Assume an attacker properly guesses (or knows) that the user's password is four characters lower-alpha. An exhaustive search attack on this password is completely feasible at 26^4!
    
    Chaff camouflages this weak hash in a field of 26^4 passwords.  Now to do an exhaustive search the attacker must try 26^4 on every one of these hashes (26^4*26^4=26^8).  
    
    Without changing anything about the hash we have made the incredibly weak password as hard to crack as an 8 character password.
    
    The trade off comes in hard disk space, the current password files take 14mb of space on the hard drive (for 128 bit md5 hashes saved as 32 bytes each).  I am planning on finding a new way to cut down on this. 
    
    Enjoy this proof of concept!

--- chaff.py
+++ chaff.py
@@ -1,26 +1,86 @@
+import sys
 import random
 import hashlib
+import os
+
+choice = 5
 
 def hash(inp):
 	a = hashlib.md5()
-	a.update(inp)
+	a.update(inp.encode('utf-8'))
 	hazh = a.hexdigest()
 	
 	cache = []
-	timez = random.randint(20,100)
-	for i in range(timez):
+	for i in range(26**4):
+		if i % 1000 == 0:
+			if os.system('cls'):
+				os.system('clear')
+			sys.stdout.write("#>")
+			for asdf in range(int(round(i/26**4,2)*10)):
+				sys.stdout.write('*')
+			print('<#')
+			print("Working.. :")
+		
 		b = hashlib.md5()
-		rand = random.randint(1000000,9999999)
-		b.update(str(rand))
+		rand = ''
+		for co in range(20):
+			rand = rand + chr(random.randint(0,127))
+		b.update(str(rand).encode('utf-8'))
 		e = b.hexdigest()
 		cache.append(e)
 	l = len(cache)
 	place = random.randint(0,l-1)
 	cache[place] = hazh
-	for i in range(l-1):
-		print str(i) + ":" + cache[i]
-	
-	print "Your hash is #%s" % place
+	print("Writing to new user passwd-file...")
+	return cache
 
-rea = raw_input("Enter a hash: ")
-hash(rea)
+def passfi(user, cache):
+	finame = "passwd-" + user
+	fi = open(finame, "w")
+	fi.close()
+	
+	stri = ''
+	for t in cache:
+		stri = stri + t + ":"
+		if len(stri) == 676*33:
+			fi = open(finame, "a")
+			fi.write(stri)
+			fi.close()
+			stri = ''
+
+def auth():
+	user = input("Username: ")
+	guess = input("Password: ")
+	h = hashlib.md5()
+	h.update(guess.encode('utf-8'))
+	hazh = h.hexdigest()
+	
+	passwdfi = open("passwd-" + user, 'r')
+	passwdl = passwdfi.read()
+	passwd = passwdl.split(':')
+	print(len(passwd))
+	
+	for h in passwd:
+		if h == hazh:
+			return True
+	
+	return False
+
+if os.system("cls"):
+	os.system("clear")
+
+print("Welcome To Chaff POC")
+print("********************")
+print()
+print("1) Create New User")
+print("2) Attempt To Login")
+print()
+
+while abs(int(choice)) > 2:
+	choice = input("Choose[1,2]: ")
+	
+if choice == "1":
+	passfi(input("Enter new username: "),hash(input("Enter new password: ")))
+else:
+	if auth():
+		print("Congrats!")
